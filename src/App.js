8285211716828fd4b8a1c39fac30d9f227003d2e
shj3497 <shj3497@gmail.com>
2021-06-28 21:04:48 +0900

    common파일

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import styles from './App.module.css'
 
-const App = (props) => (
-      
-  );
+const App = (props) => {
+
+  return(
+    <h1 className={styles.title}>Hello</h1>
+  )
+}
 
 export default App;
