46a7666864661c3305e150e97e84e530f52984a1
shj3497 <shj3497@gmail.com>
2021-06-30 22:05:36 +0900

    firebase add

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import styles from './App.module.css'
 const App = (props) => {
 
   return(
-    <h1 className={styles.title}>Hello</h1>
+    <h1 className="">Hello</h1>
   )
 }
 
---
+++ src/service/firebase.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase'
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
